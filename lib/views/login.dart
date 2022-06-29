cda113adcc748afc555e27e50d016296ca538cbe
Marya Baloch <maryabaloch7@gmail.com>
2022-06-30 00:08:15 +0500

    2nd commit

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -35,10 +35,65 @@ class _LoginState extends State<Login> {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
               decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius:
-                      BorderRadius.only(topLeft: Radius.circular(64))),
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(64),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(16),
+                            bottomLeft: Radius.circular(16),
+                            bottomRight: Radius.circular(16),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text(
+                              "Email",
+                              style: TextStyle(fontSize: 25),
+                            ),
+                            TextField(
+                              decoration: InputDecoration(
+                                hintText: "Email Address",
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "Don't have an account? ",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           )
         ],
